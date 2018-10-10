4a21f85ea104a5810d5c42a7e95ef1cbcb3700c1
Bruno Oliveira <bruno.oliveira360@gmail.com>
2018-10-10 14:30:20 +0100

    Exemplo valdiar sintaxe XML

---
+++ ExemploValidarSintaxeXML/MyErrorHandler.java
@@ -0,0 +1,38 @@
+
+package Examples;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ * Error Handler class for SAX Parser
+ * Fonte: http://www.java2s.com/Code/Java/XML/UsingDOMforSyntaxChecking.htm
+ * 
+ */
+public class MyErrorHandler implements ErrorHandler {
+  @Override
+  public void warning(SAXParseException e) throws SAXException {
+    show("Warning", e);
+    throw (e);
+  }
+
+  @Override
+  public void error(SAXParseException e) throws SAXException {
+    show("Error", e);
+    throw (e);
+  }
+  
+  @Override
+  public void fatalError(SAXParseException e) throws SAXException {
+    show("Fatal Error", e);
+    throw (e);
+  }
+
+  private void show(String type, SAXParseException e) {
+    System.out.println(type + ": " + e.getMessage());
+    System.out.println("Line " + e.getLineNumber() + " Column "
+        + e.getColumnNumber());
+    System.out.println("System ID: " + e.getSystemId());
+  }
+}
