44b4f64c8a6c5ea9be494c95289edc98fa1f9123
Bruno Oliveira <bruno.oliveira3@sapo.pt>
2018-11-15 13:11:55 +0000

    atualização exemplo xpath

---
+++ ExemploJavaXPathParser/XpathNoResultsException.java
@@ -0,0 +1,20 @@
+
+public class XpathNoResultsException extends Exception {
+
+    /**
+     * Creates a new instance of <code>XpathNoResultsException</code> without
+     * detail message.
+     */
+    public XpathNoResultsException() {
+    }
+
+    /**
+     * Constructs an instance of <code>XpathNoResultsException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public XpathNoResultsException(String msg) {
+        super(msg);
+    }
+}
